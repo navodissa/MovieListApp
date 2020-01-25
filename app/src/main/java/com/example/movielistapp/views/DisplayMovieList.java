0c261bf64b9c2da3eb86f8b849f9723194c7fee1
Navoda Dissanayake <navomails@gmail.com>
2020-01-26 02:25:56 +0530

    RecycleView with LinearView introduced

--- app/src/main/java/com/example/movielistapp/views/DisplayMovieList.java
+++ app/src/main/java/com/example/movielistapp/views/DisplayMovieList.java
@@ -19,41 +19,41 @@ import java.util.List;
 
 public class DisplayMovieList extends Activity {
 
-//    private List<Movies> movieList = new ArrayList<>();
-//    private RecyclerView recyclerView;
-//    private DataAdapter mAdapter;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-//
-//        mAdapter = new DataAdapter(movieList);
-//        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-//        recyclerView.setLayoutManager(mLayoutManager);
-//        recyclerView.setItemAnimator(new DefaultItemAnimator());
-//        recyclerView.setAdapter(mAdapter);
-//
-//        initmovieData();
-//    }
-//
-//    private void initmovieData() {
-//        Movies movie = new Movies("Hello Android", "Ed Burnette");
-//        movieList.add(movie);
-//
-//        movie = new Movies("Beginning Android 3", "Mark Murphy");
-//        movieList.add(movie);
-//
-//        movie = new Movies("Unlocking Android", " W. Frank Ableson");
-//        movieList.add(movie);
-//
-//        movie = new Movies("Android Tablet Development", "Wei Meng Lee");
-//        movieList.add(movie);
-//
-//        movie = new Movies("Android Apps Security", "Sheran Gunasekera");
-//        movieList.add(movie);
-//
-//        mAdapter.notifyDataSetChanged();
-//    }
+    private List<Movies> movieList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private DataAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_movie_list);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        mAdapter = new DataAdapter(movieList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        initmovieData();
+    }
+
+    private void initmovieData() {
+        Movies movie = new Movies("Hello Android", "Ed Burnette");
+        movieList.add(movie);
+
+        movie = new Movies("Beginning Android 3", "Mark Murphy");
+        movieList.add(movie);
+
+        movie = new Movies("Unlocking Android", " W. Frank Ableson");
+        movieList.add(movie);
+
+        movie = new Movies("Android Tablet Development", "Wei Meng Lee");
+        movieList.add(movie);
+
+        movie = new Movies("Android Apps Security", "Sheran Gunasekera");
+        movieList.add(movie);
+
+        mAdapter.notifyDataSetChanged();
+    }
 }
--- app/src/main/java/com/example/movielistapp/views/MainActivity.java
+++ app/src/main/java/com/example/movielistapp/views/MainActivity.java
@@ -31,25 +31,15 @@ public class MainActivity extends Activity {
     public static final String EXTRA_MESSAGE = "com.example.movielistapp.MESSAGE";
     private List<Movies> movieList = new ArrayList<>();
     private RecyclerView recyclerView;
+   // private RecyclerView.LayoutManager mLayoutManager;
     private DataAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-//        activityMainBinding.setViewModel(new LoginViewModel());
-//        activityMainBinding.executePendingBindings();
-
-        setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
-
-        mAdapter = new DataAdapter(movieList);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(mAdapter);
-
-        initmovieData();
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+        activityMainBinding.executePendingBindings();
     }
 
     public void sendMessage(View view){
@@ -66,23 +56,4 @@ public class MainActivity extends Activity {
             Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
     }
 
-
-    private void initmovieData() {
-        Movies movie = new Movies("Hello Android", "Ed Burnette");
-        movieList.add(movie);
-
-        movie = new Movies("Beginning Android 3", "Mark Murphy");
-        movieList.add(movie);
-
-        movie = new Movies("Unlocking Android", " W. Frank Ableson");
-        movieList.add(movie);
-
-        movie = new Movies("Android Tablet Development", "Wei Meng Lee");
-        movieList.add(movie);
-
-        movie = new Movies("Android Apps Security", "Sheran Gunasekera");
-        movieList.add(movie);
-
-        mAdapter.notifyDataSetChanged();
-    }
 }
